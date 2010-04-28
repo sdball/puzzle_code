da14bc32a4f323949670f72c7c3b3bf8752b055d
Stephen Ball <sdball@gmail.com>
2010-04-27 22:54:10 -0400

    added solution to front3
    http://codingbat.com/prob/p147920

--- codingbat/python/warmup-1.py
+++ codingbat/python/warmup-1.py
@@ -219,6 +219,25 @@ def front_back(str):
         return str
     return ''.join([str[-1], str[1:-1], str[0]])
 
+def front3(str):
+    """
+    Given a string.
+    
+    We'll say that the front is the first 3 chars of the
+    string. If the string length is less than 3, the 
+    front is whatever is there. Return a new string which
+    is 3 copies of the front. 
+    
+    >>> front3('Java')
+    'JavJavJav'
+    >>> front3('Chocolate')
+    'ChoChoCho'
+    >>> front3('abc')
+    'abcabcabc'
+    
+    """
+    return str[:3] * 3
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
