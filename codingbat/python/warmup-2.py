ed8294f9570db9dbe13b08bc05ef2cddd889e3a5
Stephen Ball <sdball@gmail.com>
2010-04-27 22:59:28 -0400

    added solution for front_times
    http://codingbat.com/prob/p165097

--- codingbat/python/warmup-2.py
+++ codingbat/python/warmup-2.py
@@ -15,6 +15,24 @@ def string_times(str, n):
     """
     return str * n
 
+def front_times(str, n):
+    """
+    Given a string and a non-negative int n.
+    
+    We'll say that the front of the string is the first 3
+    chars, or whatever is there if the string is less than
+    length 3. Return n copies of the front.
+
+    >>> front_times('Chocolate', 2)
+    'ChoCho'
+    >>> front_times('Chocolate', 3)
+    'ChoChoCho'
+    >>> front_times('Abc', 3)
+    'AbcAbcAbc'
+    
+    """
+    return str[:3] * n
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
