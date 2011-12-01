8a19943ba6fe4aa1190687cd97b51678edc62da4
Stephen Ball <sdball@gmail.com>
2011-12-01 15:14:38 -0500

    simplified level playing field character
    It looks like self.class is functionally the same
    (in this case) as the (class << self) line.

--- ruby_quiz/16/players/level_playing_field.rb
+++ ruby_quiz/16/players/level_playing_field.rb
@@ -1,7 +1,7 @@
 class LevelPlayingField < Player
   def initialize(opponent)
     # prevent cheater from winning by waiting to pick a strategy
-    (class << self; self; end).class_eval do
+    self.class.class_eval do
       def choose
         [:rock, :paper, :scissors].shuffle.first
       end
