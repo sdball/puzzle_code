92ebf9725ed6a3299d92fd1dfe86fd6d91ab9568
Stephen Ball <sdball@gmail.com>
2011-12-06 09:20:27 -0500

    switched to class_eval
    module_eval was unnecessary and less obvious

--- ruby_quiz/16/players/cheater.rb
+++ ruby_quiz/16/players/cheater.rb
@@ -1,6 +1,6 @@
 class Cheater < Player
   def initialize(opponent)
-    Kernel.const_get(opponent).module_eval('def choose; :scissors; end')
+    Kernel.const_get(opponent).class_eval('def choose; :scissors; end')
   end
 
   def choose
